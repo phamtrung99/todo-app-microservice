5e865051376c7192ec3b6bdfd983fc8ae7fcc429
Trung Pham <trung.pham@executionlab.asia>
2025-03-27 16:48:12 +0700

    Add todo serrvice

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# todo-app-microservice
-Building simple todo app using microservice technique.
+## Structure
+
+```bash
+toto-app-microservice/
+│── api-gateway/         # HTTP Gateway sử dụng Go-Micro
+│   ├── go.mod
+│   ├── main.go          # Khởi tạo HTTP server
+│   ├── handler.go       # Xử lý HTTP request
+│   ├── client.go        # Kết nối đến gRPC services
+│── todo/                # gRPC Service 1
+│   ├── go.mod
+│   ├── main.go
+│   ├── proto/
+│── task/                # gRPC Service 2
+│   ├── go.mod
+│   ├── main.go
+│   ├── proto/
+
+```
+
+
+## Start
+### I. Run manually: 
+1. Gen protoc
+
+``` bash
+protoc --proto_path=./proto --micro_out=./proto --go_out=./proto todo.proto
+```
+
+
+
+
+### Reference
+https://www.twilio.com/en-us/blog/a-practical-guide-to-creating-microservices-with-go-micro
---
+++ todo/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	pb "github.com/phamtrung99/todo-app-microservice/todo/proto"
+	"go-micro.dev/v5"
+	"go-micro.dev/v5/logger"
+)
+
+func main() {
+	service := micro.NewService(
+		micro.Name("todo.service"),
+		micro.Version("5.3.0"),
+	)
+
+	service.Init()
+
+	todoService := &TodoService{
+		todos: make(map[string]*pb.Todo),
+	}
+	pb.RegisterTodoServiceHandler(service.Server(), todoService)
+
+	if err := service.Run(); err != nil {
+		logger.Fatal(err)
+	}
+}
+
+type TodoService struct {
+	todos map[string]*pb.Todo
+}
+
+func (s *TodoService) CreateTodo(ctx context.Context, req *pb.CreateTodoRequest, rsp *pb.CreateTodoResponse) error {
+	id := fmt.Sprintf("%d", len(s.todos)+1)
+	s.todos[id] = &pb.Todo{
+		Id:          id,
+		Title:       req.Title,
+		Description: req.Description,
+	} // Correctly access fields from req
+	rsp.Id = id
+	return nil
+}
+
+func (s *TodoService) GetTodos(ctx context.Context, req *pb.GetTodosRequest, rsp *pb.GetTodosResponse) error {
+	for _, todo := range s.todos {
+		rsp.Todos = append(rsp.Todos, todo)
+	}
+	return nil
+}
